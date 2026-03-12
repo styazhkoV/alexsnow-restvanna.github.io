28e403e1f370c7224784233e561988434b9f168b
alexsnow <alexsandr.styazhkov@gmail.com>
2026-03-12 13:49:51 +0500

    modified:   astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://styazhkov.github.io', // Замените на ваш URL GitHub Pages
-  base: '/restvanna', // Замените на имя вашего репозитория (если деплой не в корень)
+  base: '/', // Замените на имя вашего репозитория (если деплой не в корень)
   integrations: [tailwind()],
 });
