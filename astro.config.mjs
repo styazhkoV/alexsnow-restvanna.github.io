795f78b4c70e4bd8e2100ee2ba9955d1f807a6de
styazhkoV <alexsandr.styazhkov@gmail.com>
2026-06-27 20:08:03 +0500

    Изменены цены

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 // astro.config.mjs
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   site: 'https://styazhkov.github.io',
   base: '/alexsnow-restvanna.github.io', // <--- Добавь название репо
