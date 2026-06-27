53b09e363af66902c1117398234d2e078262a9d5
styazhkoV <alexsandr.styazhkov@gmail.com>
2026-06-27 20:24:34 +0500

    Попрравил деплой

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
-// astro.config.mjs
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+
 export default defineConfig({
-  site: 'https://styazhkov.github.io',
-  base: '/alexsnow-restvanna.github.io', // <--- Добавь название репо
   integrations: [tailwind()],
+  site: "https://styazhkoV.github.io",
+  base: "/alexsnow-restvanna.github.io",
 });
