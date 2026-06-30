42e121dfff6354d457ebb8a1255a9c64f1c55574
styazhkoV <alexsandr.styazhkov@gmail.com>
2026-06-30 20:37:51 +0500

    Add GitHub Pages deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-
 export default defineConfig({
-  integrations: [tailwind()],
-  site: "https://styazhkoV.github.io",
+  site: "https://styazhkoV.github.io/alexsnow-restvanna.github.io",
   base: "/alexsnow-restvanna.github.io",
+  output: "static",
+  integrations: [tailwind(), sitemap()],
 });
